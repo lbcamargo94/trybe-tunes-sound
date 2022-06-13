c3fbd7ed882064d52101ad8a2f7f447d4b413de0
lbcamargo94 <lb.camargo94@gmail.com>
2022-06-13 08:42:28 -0300

    fix: resolved react-router-sound v6 errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Routes from './routes'
+import NavigationRoutes from './routes'
 import './styles/App.css';
 
 function App() {
   return (
     <>
-      <Routes />
+      <NavigationRoutes />
     </>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import { Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-export default function Routes() {
+export default function NavigationRoutes() {
   return (
-    <div>
-      <Switch>
+    <BrowserRouter>
+      <Routes>
         <Route exact path="/" component={ "Login" } />
-        {/* <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" component={ NotFound } /> */}
-      </Switch>
-    </div>
+        <Route path="/search" component={ "Search" } />
+        <Route path="/album/:id" component={ "Album" } />
+        <Route path="/favorites" component={ "Favorites" } />
+        <Route exact path="/profile" component={ "Profile" } />
+        <Route path="/profile/edit" component={ "ProfileEdit" } />
+        <Route path="*" component={ "NotFound" } />
+      </Routes>
+    </BrowserRouter>
   )
 }
